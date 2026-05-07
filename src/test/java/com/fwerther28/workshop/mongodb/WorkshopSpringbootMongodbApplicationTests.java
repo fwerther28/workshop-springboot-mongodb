ac5087bc158d1996a72ade4e06b019c652ff79a4
Francisco Wherther <wsiriano83@gmail.com>
2026-05-07 20:55:17 -0300

    Initial commit: Spring Boot structure with MongoDB

---
+++ src/test/java/com/fwerther28/workshop/mongodb/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwerther28.workshop.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
