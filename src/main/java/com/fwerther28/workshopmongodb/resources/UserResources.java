e13e46956fdb7c89694cbfe087e6f68554d5c1ef
Francisco Wherther <wsiriano83@gmail.com>
2026-05-26 23:03:59 -0300

    feat: implement user deletion endpoint with HTTP DELETE
    - Add delete method to UserService reusing findById validation
    - Create DELETE endpoint in UserResources mapping to /user/{id}
    - Return HTTP 204 No Content status upon sucessful deletion

--- src/main/java/com/fwerther28/workshopmongodb/resources/UserResources.java
+++ src/main/java/com/fwerther28/workshopmongodb/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,11 @@ public class UserResources {
 			java.net.URI uri = org.springframework.web.servlet.support.ServletUriComponentsBuilder
 					.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 			return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/fwerther28/workshopmongodb/services/UserService.java
+++ src/main/java/com/fwerther28/workshopmongodb/services/UserService.java
@@ -33,4 +33,9 @@ public class UserService {
 	public User fromDTO(com.fwerther28.workshopmongodb.dto.UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+} 
