82182b5eca43475b77134e23312d729e5cff2834
Francisco Wherther <wsiriano83@gmail.com>
2026-06-13 19:57:23 -0300

    feat: implement custom MongoDB JSON query using @Query annotation

--- src/main/java/com/fwerther28/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/fwerther28/workshopmongodb/resources/PostResource.java
@@ -23,10 +23,15 @@ public class PostResource {
 	
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
-		
-		text = URL.decodeParam(text);
-		
+		text = URL.decodeParam(text);	
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/titlesearchquery")
+	public ResponseEntity<List<Post>> searchTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);	
+		List<Post> list = service.searchTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
 }
